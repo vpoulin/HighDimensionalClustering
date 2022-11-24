1e8e9809561fe2af9660d565d84ee92005e6d33e
acwooding <amy@wooding.org>
2022-11-24 10:28:39 -0500

    initial easydata import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='2.0.0',
+    description='''Explore solutions to high dimensional clustering''',
+    author='vpoulin',
+    license='MIT',
+)
